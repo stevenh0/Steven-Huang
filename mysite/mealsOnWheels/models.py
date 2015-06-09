6214c10335805e861e7675db0605db848aeb6495
SPPH <spph@dhcp1.spph.ubc.ca>
2015-06-09 11:56:23 -0700

    Updated models, added foreign key for Position field and added string methods to facilitate testing

--- mysite/mealsOnWheels/models.py
+++ mysite/mealsOnWheels/models.py
@@ -26,16 +26,24 @@ class Choice(models.Model):
 
 ## The above will deleted later
 
+## Definitions for Food Truck-related classes
+
 class Position(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
+
+    def __str__(self):
+        return "(" + str(self.lat) + "," + str(self.lon) + ")"
 
 class FoodTruck(models.Model):
     key = models.CharField('Key',max_length=50)
     key.primary_key = True
     name = models.CharField('Truck Name', max_length=200)
     foodType = models.CharField('Truck Food Type', max_length=200)
-    position = Position()
+    position = models.ForeignKey(Position)
+
+    def __str__(self):
+        return "Truck " + self.key + ": " + self.name
 
 ## Needed for authentifiation
 from django.contrib.auth.models import User
