21f365755233e9ef06b86dade41858f5d253e502
Steven Huang <stevenh0@hotmail.com>
2015-06-09 21:12:51 -0700

    updated registration tests

--- mysite/mealsOnWheels/tests.py
+++ mysite/mealsOnWheels/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.core import mail
 #from .forms import UserForm
-from parser import testImportData, clearData, updateJSONObject
+from parser import testImportData, clearData
 from models import FoodTruck, Position
 
 # Create your tests here.
@@ -10,9 +11,9 @@ from models import FoodTruck, Position
 
 # Test that only by entering the appropriate information into the fields will you be able to login
 
-"""
 
-class LoginViewTests(TestCase):
+
+class RegisterViewTests(TestCase):
 
     def test_login_render(self):
         response = self.client.get(reverse('mealsOnWheels:register'))
@@ -21,59 +22,111 @@ class LoginViewTests(TestCase):
     def test_missing_username(self):
         params = {
                'email': 'stevenh0@hotmail.com',
-               'password:': 'hello',}
+               'password1': 'hello',
+               'password2': 'hello'}
 
         response = self.client.post(reverse('mealsOnWheels:register'), params)
         self.assertEqual(response.status_code, 200)
-        self.assertFormError(response, "user_form","username","This field is required.")
+        self.assertFormError(response, "form","username","This field is required.")
 
 
- # TODO: email field seems not to cause an error when left blank
- #   def test_missing_email(self):
- #       params = {'username': 'steven',
- #              'password:': 'hello',}
- #       response = self.client.post(reverse('mealsOnWheels:register'), params)
- #       self.assertEqual(response.status_code, 200)
- #       self.assertFormError(response, "user_form","email","This field is required.")
+
+    def test_missing_email(self):
+        params = {'username': 'steven',
+               'password1': 'hello',
+               'password2': 'hello'}
+        response = self.client.post(reverse('mealsOnWheels:register'), params)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, "form","email","This field is required.")
 
     def test_missing_password(self):
         params = {'username': 'steven',
-               'email': 'stevenh0@hotmail.com',}
+               'email': 'stevenh0@hotmail.com',
+               'password2:': 'hello'}
         response = self.client.post(reverse('mealsOnWheels:register'), params)
         self.assertEqual(response.status_code, 200)
-        self.assertFormError(response, "user_form","password","This field is required.")
+        self.assertFormError(response, "form","password1","This field is required.")
 
-# TODO: tooltip pops up on page not sure how to test
-#    def test_invalid_email(self):
-#        params = {'username': 'steven',
-#                  'email': 'asdfasdf',
-#               'password:': 'hello',}
-#        response = self.client.post(reverse('mealsOnWheels:register'), params)
-#        self.assertEqual(response.status_code, 200)
-#        self.assertFormError(response, "user_form","email","This field is required.")
+
+    def test_invalid_email(self):
+        params = {'username': 'steven',
+                  'email': 'asdfasdf',
+               'password1': 'hello',
+               'password2': 'hello'}
+        response = self.client.post(reverse('mealsOnWheels:register'), params)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, "form","email","Enter a valid email address.")
 
 
 
     def test_complete_form(self):
+         response1 = self.client.get(reverse('mealsOnWheels:register'))
+         self.assertContains(response1, 'Create a new account')
          params = {'username': 'steven',
                    'email': 'stevenh0@hotmail.com',
-                   'password': 'asdf'}
-         response = self.client.post(reverse('mealsOnWheels:register'), params)
-         self.assertContains(response, 'You are already registered')
+                   'password1': 'asdf',
+                   'password2': 'asdf'}
+         self.assertEqual(len(mail.outbox), 0)
+         response2 = self.client.post(reverse('mealsOnWheels:register'), params)
+         self.assertContains(response2, 'To complete the registration')
+
+         # all emails sent are redirected to dummy outbox, if outbox contains an email, confirmation was sent
+
+         self.assertEqual(len(mail.outbox), 1)
 
     def test_duplicate_user(self):
+        params1 = {'username': 'steven',
+                   'email': 'stevenh0@hotmail.com',
+                   'password1': 'asdf',
+                   'password2': 'asdf'}
+        params2 = {'username': 'steven',
+                   'email': 'stevenh1@hotmail.com',
+                   'password1': 'asdf',
+                   'password2': 'asdf'}
+        self.client.post(reverse('mealsOnWheels:register'), params1)
+        response = self.client.post(reverse('mealsOnWheels:register'), params2)
+        self.assertFormError(response, "form","username","A user with that username already exists.")
+
+
+    def test_duplicate_email(self):
+        params1 = {'username': 'steven1',
+                   'email': 'stevenh0@hotmail.com',
+                   'password1': 'asdf',
+                   'password2': 'asdf'}
+        params2 = {'username': 'steven2',
+                   'email': 'stevenh0@hotmail.com',
+                   'password1': 'asdf',
+                   'password2': 'asdf'}
+        self.client.post(reverse('mealsOnWheels:register'), params1)
+        response = self.client.post(reverse('mealsOnWheels:register'), params2)
+        self.assertFormError(response, "form","email","duplicate email")
+
+
+
+# TODO: rewrite test for complete form so that it doesn't check redirect based on text on page
+
+# -------- Login View Tests -----------
+
+class LoginViewTests(TestCase):
+
+    def test_invalid_login(self):
+        params = {'username':'',
+                  'password':''}
+        response = self.client.post(reverse('mealsOnWheels:login'), params)
+        self.assertContains(response, 'Invalid username or password')
+
+"""
+    def test_login(self):
         params = {'username': 'steven',
                    'email': 'stevenh0@hotmail.com',
-                   'password': 'asdf'}
+                   'password1': 'asdf',
+                   'password2': 'asdf'}
         self.client.post(reverse('mealsOnWheels:register'), params)
-        response = self.client.post(reverse('mealsOnWheels:register'), params)
-        self.assertFormError(response, "user_form","username","A user with that username already exists.")
+ """
 
-        """
-
-# TODO: make duplicate emails return warning
-# TODO: make blank email page return warning
-# TODO: write test to see if emails are being sent properly
+        
+# TODO: Test login works. Issue: how to mimic email confirmation?
+# TODO: Remove Register and Login buttons from home page after logging in with a user?
 
 # -------- Import Data Tests ---------
 
@@ -105,9 +158,6 @@ class ImportDataTests(TestCase):
     self.assertEquals(FoodTruck.objects.filter(foodType="Hot Dogs").count(), 2)
     self.assertEquals(FoodTruck.objects.get(key="c3").foodType, "Fish Tacos")
     self.assertEquals(FoodTruck.objects.get(name="Angry Al's").foodType, "Mystery Food")
-
-  def testWriteToJSON(self):
-    updateJSONObject()
 
   def testDataGoneAfterBeingCleared(self):
     clearData()
