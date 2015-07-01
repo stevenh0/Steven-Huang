4d2344c32126bed4d96d9d9d15878859ca0bdd64
Yumi FakeName <yumik091186@gmail.com>
2015-06-30 19:55:39 -0700

    a user landmark can be added through the search bar in the map

--- mysite/mealsOnWheels/static/mealsOnWheels/js/foodtruckmap.js
+++ mysite/mealsOnWheels/static/mealsOnWheels/js/foodtruckmap.js
@@ -2,6 +2,7 @@
 var map;
 var downtownVancouver = new google.maps.LatLng(49.28,-123.12);
 
+
 function initialize() {
   var mapOptions = {
     zoom: 14,
@@ -10,6 +11,69 @@ function initialize() {
   }
 
 map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+// https://developers.google.com/maps/documentation/javascript/markers
+// https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+// Create the search box and link it to the UI element.
+
+var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+var markers = [];
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location,
+        click: true,
+        draggable: true,
+        animation: google.maps.Animation.BOUNCE// DROP
+      });
+      markers.push(marker);
+      // Zoom in/out to show both the new location and the food vendors
+      bounds.extend(downtownVancouver);
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+    // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+
+
+
 
 $(document).ready(function() {
     $.getJSON("/mealsOnWheels/food_trucks", function(food_trucks_json) {
