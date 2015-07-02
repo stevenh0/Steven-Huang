4a83ae954b7ead6c7ec24d7737a666f37f4c0fe7
Yumi FakeName <yumik091186@gmail.com>
2015-07-02 11:38:16 -0700

    odvious comments and printing statements are deleted

--- mysite/mealsOnWheels/views.py
+++ mysite/mealsOnWheels/views.py
@@ -12,22 +12,13 @@ def index(request):
 
 
 def importData():
-    # Persing .xcel file
-    ## FTP into the web server
     ftp = FTP('webftp.vancouver.ca')
-    ## Log-in as anonymous user
     ftp.login()
-    ## change directory into xls
     ftp.cwd('OpenData/xls')
-    ## File name of the xls file is:
     filename = 'new_food_vendor_locations.xls'
-    ## Retrive the xls file and save it as myLovelyNewFile.xls into my current directory
     ftp.retrbinary('RETR %s' % filename, open('myLovelyNewFile.xls', 'w').write)
-    ## Quit ftp
     ftp.quit()
-    ## Now read the myLovelyNewFile.xls from my current directry
     workbook = xlrd.open_workbook('myLovelyNewFile.xls')
-    ## The first sheet contains our food data
     worksheet = workbook.sheet_by_name('Query_vendor_food')
     return worksheet
 
@@ -40,39 +31,23 @@ from django.http import HttpResponseRedirect,HttpResponse
 
 
 def user_login(request):
-    print("user_login inside")
     ## If the request is a HTTP POST, try to pull out the relevant infomation
     if request.method == 'POST':
-        ## Gather the username and password provided by the users
-        ## This information is obtained from the login form
         username = request.POST['username']
-        print("username: "+ username)
         password = request.POST['password']
-        print("password: "+ password)
         user = authenticate(username=username,password=password)
-
-        ## If we have a user object, the details are correct.
-        ## If None (Python's way of representing the absence of a value), no user with
-        ## matching credentials are found
         if user:
-            print("user.is_active: "+ str(user.is_active))
             if user.is_active:
-
                 login(request,user)
-                print("login(request,user) is passed!???!")
                 return HttpResponseRedirect('/mealsOnWheels/')
             else:
                 message="disable"
                 return render(request,'mealsOnWheels/login.html',{"message":message})
-                ##return HttpResponse("Your meals on wheels account is disabled.")
         else:
             message="invalid"
-            print "Invalid login details: {0}, {1}".format(username,password)
             return render(request,'mealsOnWheels/login.html',{"message":message})
-            ##HttpResponse("Invalid login details are shipped")
     else:
         message="work"
-        #return HttpResponse("Wow")#
         return render(request,'mealsOnWheels/login.html',{"message":message})
 
 
@@ -103,19 +78,18 @@ from .models import *
 import hashlib, datetime, random
 from django.core.context_processors import csrf
 
+## Send email reference
+## http://www.mangooranges.com/2008/09/15/sending-email-via-gmail-in-django/
 def register_user(request):
     args = {}
     args.update(csrf(request))
-    print "register_user: request.method " + str(request.method)
 
     if request.method == 'POST':
         form = RegistrationForm(data=request.POST)
         args['form'] = form
 
         if form.is_valid():
-            print "form.is_valid() passed"
             form.save() ## save the registration form (overriden in form.py)
-            print "form.save() passed"
             ## saved user is NOT ACTIVE YET
             ## Necessary for the confirmation email
             username = form.cleaned_data['username']
@@ -135,13 +109,11 @@ def register_user(request):
             new_profile = UserProfile(user=user,activation_key=activation_key,key_expires=key_expires)
             new_profile.save()
 
-            ## Send email reference
-            ## http://www.mangooranges.com/2008/09/15/sending-email-via-gmail-in-django/
+
             Subject = 'Account confirmation for Meals on Wheels'
             Body1 = 'Hello %s, \n\nThank you very much for signing up. ' % (username)
             Body2 = 'To activate your account, click this link below within 24 hours: '
-            ## Body 3 will be omitted in the future
-            Body3 = '\nhttp://127.0.0.1:8000/mealsOnWheels/confirm/%s' % (activation_key)
+            ## Body3 = '\nhttp://127.0.0.1:8000/mealsOnWheels/confirm/%s' % (activation_key)
             Body4 = '\nhttp://djanguars.pythonanywhere.com/mealsOnWheels/confirm/%s' % (activation_key)
             email = EmailMessage(Subject, Body1+Body2+Body3 + Body4, to=[email])
             email.send()
